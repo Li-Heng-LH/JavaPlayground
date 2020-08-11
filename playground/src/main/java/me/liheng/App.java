4bbc9184283d22b1163cef0c56d0256f74368652
Li Heng <liheng6567@gmail.com>
2020-08-11 23:32:07 +0800

    Able to generate pom.xml and remove if needed

--- playground/src/main/java/me/liheng/App.java
+++ playground/src/main/java/me/liheng/App.java
@@ -6,6 +6,6 @@ public class App {
 
     public static void main( String[] args ) throws IOException, InterruptedException {
 
-        CFRRunner.run();
+        CommandRunner.run();
     }
 }
--- playground/src/main/java/me/liheng/CommandRunner.java
+++ playground/src/main/java/me/liheng/CommandRunner.java
@@ -4,6 +4,9 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 //Tutorial to run a Shell Command in Java
 public class CommandRunner {
@@ -11,9 +14,25 @@ public class CommandRunner {
     public static void run() throws IOException, InterruptedException {
         System.out.println(System.getProperty("os.name").toLowerCase());
 
+        Path originalPath = Paths.get("");
+        boolean needToDeletePom = false;
+        if (! new File("6.13.0/pom.xml").exists()) {
+            File dir = new File("6.13.0");
+            for (File file : dir.listFiles()) {
+                if (file.getName().toLowerCase().endsWith((".pom"))) {
+                    originalPath = file.toPath();
+                    break;
+                }
+            }
+            Path copiedPomPath = Paths.get("6.13.0/pom.xml");
+            Files.copy(originalPath, copiedPomPath);
+            needToDeletePom = true;
+        }
+
+
         ProcessBuilder builder = new ProcessBuilder();
-        builder.command("mvn", "dependency:list");
         builder.directory(new File("6.13.0"));
+        builder.command("mvn", "dependency:list");
         Process process = builder.start();
 
         BufferedReader r = new BufferedReader(new InputStreamReader(process.getInputStream()));
@@ -26,7 +45,12 @@ public class CommandRunner {
         int exitCode = process.waitFor();
         assert exitCode == 0;
 
-        System.out.println("End of shell cmd");
+
+        if (needToDeletePom) {
+            Files.delete(Paths.get("6.13.0/pom.xml"));
+            needToDeletePom = false;
+        }
+
 
     }
 
