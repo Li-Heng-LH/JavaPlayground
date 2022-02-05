9dc1c2f50977c46b0dde4c37b9d447234e5e4064
Li Heng <liheng6567@gmail.com>
2022-02-06 00:18:28 +0800

    Solace: prove directPublisher can publish to both queueReceiver and directReceiver

--- try-solace/src/main/java/me/liheng/cloud/DirectToQueueDemo.java
+++ try-solace/src/main/java/me/liheng/cloud/DirectToQueueDemo.java
@@ -1,11 +1,12 @@
 package me.liheng.cloud;
 
 import com.solace.messaging.publisher.DirectMessagePublisher;
-import com.solace.messaging.publisher.OutboundMessageBuilder;
+import com.solace.messaging.receiver.DirectMessageReceiver;
 import com.solace.messaging.receiver.MessageReceiver;
 import com.solace.messaging.receiver.PersistentMessageReceiver;
 import com.solace.messaging.resources.Queue;
 import com.solace.messaging.resources.Topic;
+import com.solace.messaging.resources.TopicSubscription;
 
 import java.io.IOException;
 
@@ -20,22 +21,33 @@ public class DirectToQueueDemo {
 
         final DirectMessagePublisher directPublisher = CloudUtil.getMessagingService()
                 .createDirectMessagePublisherBuilder().onBackPressureWait(1).build().start();
-        //OutboundMessageBuilder messageBuilder = CloudUtil.getMessagingService().messageBuilder();
         final PersistentMessageReceiver queueReceiver = CloudUtil.getMessagingService()
                 .createPersistentMessageReceiverBuilder()
                 .withMessageAutoAcknowledgement()
                 .build(Queue.durableExclusiveQueue("queue2")).start();
+        final DirectMessageReceiver directReceiver = CloudUtil.getMessagingService().
+                createDirectMessageReceiverBuilder()
+                .withSubscriptions(TopicSubscription.of("persistent/two"))
+                .build().start();
         final MessageReceiver.MessageHandler messageHandler = (inboundMessage) ->
-                System.out.printf("< Received message on thread: %s : %s %n",
-                        Thread.currentThread().getId(), inboundMessage.getPayloadAsString());
+                System.out.printf("< Received message on thread: %s from %s: %s %n",
+                        Thread.currentThread().getId(),
+                        inboundMessage.getDestinationName(),
+                        inboundMessage.getPayloadAsString());
+
 
         directPublisher.publish("Msg 1", Topic.of("persistent/two"));
         System.out.println("> Published 'Msg 1' to persistent/two");
         queueReceiver.receiveAsync(messageHandler);
-        Thread.sleep(1000);
+        directReceiver.receiveAsync(messageHandler);
 
+
+        Thread.sleep(1000);
         directPublisher.terminate(500);
         queueReceiver.terminate(500);
+        directReceiver.terminate(500);
         CloudUtil.getMessagingService().disconnect();
+
+        //TODO: test if can send to queue directly?
     }
 }
