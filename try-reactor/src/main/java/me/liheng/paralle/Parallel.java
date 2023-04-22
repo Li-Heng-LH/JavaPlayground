e39a23ee79f0bf830849691ab3457b798a50d876
Li Heng <liheng6567@gmail.com>
2023-04-22 15:23:49 +0800

    Parallel Flux works as expected

--- try-reactor/src/main/java/me/liheng/paralle/Parallel.java
+++ try-reactor/src/main/java/me/liheng/paralle/Parallel.java
@@ -4,24 +4,43 @@ import reactor.core.publisher.Flux;
 import reactor.core.scheduler.Scheduler;
 import reactor.core.scheduler.Schedulers;
 
+import java.util.*;
 import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
 
 public class Parallel {
 
     static Scheduler scheduler = Schedulers.fromExecutor(Executors.newFixedThreadPool(2));
+    static AtomicInteger batchNumber = new AtomicInteger(0);
 
     public static void main(String[] args) {
         Flux.range(1, 1_000_000)
-                .buffer(1_000)
+                .buffer(10_000)
                 .parallel(2, 2)
                 .runOn(scheduler, 1)
-                //.map()
+                .map(Parallel::processBatch)
                 .flatMap(Flux::fromIterable)
                 .sequential()
                 .subscribe();
     }
 
+    private static Set<Integer> processBatch(List<Integer> batch) {
+        System.out.println(Thread.currentThread().getName() + ": Processing Batch: " + batchNumber.addAndGet(1));
+        Map<Integer, Integer> map = batch.stream()
+                .collect(Collectors.toMap(i -> i, i -> i));
+        System.out.println(Thread.currentThread().getName() + ": Map size: " + map.size());
+        return map.keySet();
+    }
+
     private static void shutDown() {
         scheduler.dispose();
     }
+
+    /* res:
+    pool-1-thread-1: Processing Batch: 6
+    pool-1-thread-2: Processing Batch: 7
+    pool-1-thread-1: Map size: 10000
+    pool-1-thread-2: Map size: 10000
+     */
 }
