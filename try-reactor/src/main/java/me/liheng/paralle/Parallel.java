da3bef40dd321363b2b1d3c09bcaac3355df4446
Li Heng <liheng6567@gmail.com>
2023-04-22 14:37:59 +0800

    Parallel flux

--- try-reactor/src/main/java/me/liheng/paralle/Parallel.java
+++ try-reactor/src/main/java/me/liheng/paralle/Parallel.java
@@ -1,11 +1,27 @@
 package me.liheng.paralle;
 
 import reactor.core.publisher.Flux;
+import reactor.core.scheduler.Scheduler;
+import reactor.core.scheduler.Schedulers;
+
+import java.util.concurrent.Executors;
 
 public class Parallel {
 
+    static Scheduler scheduler = Schedulers.fromExecutor(Executors.newFixedThreadPool(2));
+
     public static void main(String[] args) {
-        Flux.range(1, 3)
-                .subscribe(System.out::println);
+        Flux.range(1, 1_000_000)
+                .buffer(1_000)
+                .parallel(2, 2)
+                .runOn(scheduler, 1)
+                //.map()
+                .flatMap(Flux::fromIterable)
+                .sequential()
+                .subscribe();
+    }
+
+    private static void shutDown() {
+        scheduler.dispose();
     }
 }
